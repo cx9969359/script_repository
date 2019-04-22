e49696f8db7275c3fd2820dbfdde2eba35e6ad49
Chang <cx9969359@gmail.com>
2019-04-22 11:03:33 +0800

    no message

--- fastlabel_core/api/open_slide.py
+++ fastlabel_core/api/open_slide.py
@@ -1,6 +1,5 @@
 import os
 from io import BytesIO
-from flask_login import login_required
 
 from flask import make_response
 from flask_restplus import Namespace, Resource
@@ -17,7 +16,6 @@ Overlap = 1
 
 @api.route('/thumbnail/<int:image_id>')
 class Thumbnail(Resource):
-    @login_required
     def get(self, image_id):
         image = ImageModel.objects.filter(id=image_id).first()
         if not image:
