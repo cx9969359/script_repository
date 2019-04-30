ee93a6fe182e1a437ab7c21ce2c012e4263321dd
Chang <cx9969359@gmail.com>
2019-04-30 18:04:21 +0800

    no message

--- fastlabel_core/my_test_note.py
+++ fastlabel_core/my_test_note.py
@@ -1,19 +1,12 @@
-import ast
-import os
-import random
-import random
-import threading
-import time
-import time
-from multiprocessing import Process, Queue, current_process, freeze_support
 import hashlib
+import time
 
 
 def work():
     with open('F:\\tif_images\\thyroid\\more.tif', 'rb') as f:
         md5_obj = hashlib.md5()
         while True:
-            d = f.read(8096)
+            d = f.read(102400)
             if not d:
                 break
             md5_obj.update(d)
@@ -24,6 +17,6 @@ def work():
 
 
 if __name__ == '__main__':
+    start = time.time()
     work()
-
-# {'hsil':[[x1,y1,x2,y2,conf],[x1,x2],[]]}
+    print(time.time() - start)
