9fd9dd12d316d2d3e54c76dab7b70930b7e08c14
Chang <cx9969359@gmail.com>
2019-05-09 11:16:25 +0800

    no message

--- fastlabel_core/my_test_note.py
+++ fastlabel_core/my_test_note.py
@@ -11,16 +11,7 @@ def work():
     # slide = OpenSlide(path)
     # image_shape = (slide.dimensions[1],slide.dimensions[0])
     # print(image_shape)
-    path = 'E:\\L201903670.tif.txt'
-    a = {'hsil': [], 'lsil': [[1, 23, 3, 45], [3, 4, 665, 65]]}
-    for category, bbox_list in a.items():
-        print(category)
-        for bbox in bbox_list:
-            x_min, y_min, x_max, y_max = bbox[0], bbox[1], bbox[2], bbox[3]
-            coordinate_list = [{'x': x_min, 'y': y_min}, {'x': x_min, 'y': y_max}, {'x': x_max, 'y': y_min},
-                               {'x': x_max, 'y': y_max}]
-
-
+    pass
 
 if __name__ == '__main__':
     work()
